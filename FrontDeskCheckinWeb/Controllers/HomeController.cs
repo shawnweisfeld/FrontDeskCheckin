479aebe4263a60d9b3a29b35acf6bf3881cdbaab
shawnweisfeld <shawn@shawnweisfeld.com>
2015-09-20 09:25:36 -0500

    Home page list buildings

--- FrontDeskCheckinWeb/Controllers/HomeController.cs
+++ FrontDeskCheckinWeb/Controllers/HomeController.cs
@@ -4,14 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FrontDeskCheckinWeb.ViewModels.Home;
+using System.Data.Entity;
+using System.Threading.Tasks;
 
 namespace FrontDeskCheckinWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private AppContext db = new AppContext();
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var vm = new Index();
+
+            vm.Buildings = await db.Visitors
+                .GroupBy(x => x.Terminal.Building)
+                .Select(x => new BuildingDetail()
+                {
+                    Name = x.Key,
+                    TotalVisitors = x.Count(),
+                    FirstVisitor = x.Min(y => y.ArrivedAt),
+                    LastVisitor = x.Max(y => y.ArrivedAt)
+                })
+                .ToListAsync();
+
+            foreach (var building in vm.Buildings)
+            {
+                var buildingOpen = building.LastVisitor.Subtract(building.FirstVisitor);
+                building.AverageDailyVisitors = building.TotalVisitors / buildingOpen.TotalDays;
+                building.AverageMonthlyVisitors = building.TotalVisitors / (buildingOpen.TotalDays / 30);
+            }
+
+            return View(vm);
         }
 
         public ActionResult About()
@@ -26,6 +51,15 @@ namespace FrontDeskCheckinWeb.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ FrontDeskCheckinWeb/ViewModels/Home/Index.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDeskCheckinWeb.ViewModels.Home
+{
+    public class Index
+    {
+        public List<BuildingDetail> Buildings { get; set; }
+    }
+
+    public class BuildingDetail
+    {
+        public string Name { get; set; }
+        public int TotalVisitors { get; set; }
+        public double AverageDailyVisitors { get; set; }
+        public double AverageMonthlyVisitors { get; set; }
+        public DateTime FirstVisitor { get; set; }
+        public DateTime LastVisitor { get; set; }
+    }
+}
